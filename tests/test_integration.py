6d73bc459e33643d302603870f9d818842e0c875
Daniel Huppmann <dh@dergelbesalon.at>
2024-02-15 07:06:24 +0100

    Add an integration test for consistency with common-definitions

---
+++ tests/test_integration.py
@@ -0,0 +1,27 @@
+import pathlib
+import yaml
+
+import nomenclature
+
+
+def test_integration_common_definitions():
+
+    config_file = "../definitions/nomenclature.yaml"
+    config = {
+        "repositories": {
+            "common-definitions": {
+                "url": "https://github.com/IAMconsortium/common-definitions.git/"
+            }
+        },
+        "definitions": {
+            "region": {"repository": "common-definitions"},
+            "variable": {"repository": "common-definitions"},
+        },
+    }
+
+    with open(config_file, "w") as file:
+        yaml.dump(config, file)
+
+    nomenclature.DataStructureDefinition("../definitions")
+
+    pathlib.Path(config_file).unlink()
